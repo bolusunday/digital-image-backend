0246cff04660cad1be17aeea75909e09bf26ed19
bolusunday <adebolusunday86@gmail.com>
2026-08-16 00:58:05 +0100

    Hardcode production API fallback in config.js

--- digital-image-frontend/src/config.jsx
+++ digital-image-frontend/src/config.jsx
@@ -1,3 +1,5 @@
 // src/config.js
 export const API_URL =
-  import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+  import.meta.env.MODE === "production"
+    ? "https://api.pegty.com"
+    : import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
