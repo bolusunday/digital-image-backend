c310be5e456eddb31636cade37f8bdf7550f7e52
bolusunday <adebolusunday86@gmail.com>
2026-08-19 12:41:42 +0100

    Footer.jsx updated

--- digital-image-frontend/src/components/Footer.jsx
+++ digital-image-frontend/src/components/Footer.jsx
@@ -112,13 +112,13 @@ export default function Footer() {
                   to="/category/medieval"
                   className="text-zinc-400 hover:text-white transition-colors no-underline block py-0.5"
                 >
-                  Home Decor
+                  Medieval Packs
                 </Link>
                 <Link
                   to="/category/homedecor"
                   className="text-zinc-400 hover:text-white transition-colors no-underline block py-0.5"
                 >
-                  Medieval Packs
+                  Home Decor
                 </Link>
               </li>
               <li>
